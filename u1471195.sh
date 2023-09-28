0d7c28bc0fc33be838a3352aa8d051b13744b471
Dhruv Meduri <dhruvm@Dhruvs-MacBook-Air.local>
2023-09-27 23:14:30 -0600

    Third Commit

--- u1471195.sh
+++ u1471195.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=8:00:00
-#SBATCH --mem=100GB
+#SBATCH --mem=0
 #SBATCH --mail-user=u1471195@umail.utah.edu
 #SBATCH --mail-type=FAIL,END
 #SBATCH -o assignment_3-%j

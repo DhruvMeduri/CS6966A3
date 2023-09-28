57201c773fd185506b8adad2a039e2bcabb73ee2
Dhruv Meduri <dhruvm@Dhruvs-MacBook-Air.local>
2023-09-28 00:26:31 -0600

    Third Commit

--- assignment_3.py
+++ assignment_3.py
@@ -65,7 +65,7 @@ class ExplainableTransformerPipeline():
         
         attributes, delta = lig.attribute(inputs=inputs,
                                   baselines=baseline,
-                                  n_steps= 25,
+                                  n_steps= 10,
                                   target = self.__pipeline.model.config.label2id[prediction[0]['label']], 
                                   return_convergence_delta = True)
         # Give a path to save

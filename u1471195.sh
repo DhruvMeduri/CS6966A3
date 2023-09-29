a1ac1ddd09f9218cdcf71864b76ef9212ec19646
Dhruv Meduri <dhruvm@dhruvs-air.uconnect.utah.edu>
2023-09-29 10:24:47 -0600

    Test

--- u1471195.sh
+++ u1471195.sh
@@ -22,4 +22,4 @@ export HF_DATASETS_CACHE="/scratch/general/vast/u1471195/huggingface_cache"
 
 OUT_DIR=/scratch/general/vast/u1471195/cs6966/assignment3/models
 mkdir -p ${OUT_DIR}
-python ~/CS6966A3/assignment_3.py --model_checkpoint /scratch/general/vast/u1471195/cs6966/assignment1/models/deberta-v3-large/checkpoint-5000
+python ~/CS6966A3/assignment_3.py --model_checkpoint /scratch/general/vast/u1471195/cs6966/assignment1/models/deberta-v3-large/checkpoint-5001
